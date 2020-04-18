d1cf4f928228d98564403d8588569de52f448fed
Clerley <clerley@yahoo.com>
2020-04-17 23:22:12 -0400

    Working on the UI
    Signed-off-by: Clerley <clerley@yahoo.com>

--- authfe/lib/i18n/language.dart
+++ authfe/lib/i18n/language.dart
@@ -5,16 +5,24 @@ const LANG_ENGLISH = "En";
 const LANG_SPANISH = "Sp";
 
 Map<String, Map<String, String>> languages = {
-    LANG_ENGLISH: {"hello"    : "Hello",
-                   "title"    : "Novare Security Systems",
-                   "username" : "Username",
-                   "password" : "Password"
+    LANG_ENGLISH: {"hello"     : "Hello",
+                   "title"     : "Novare Security Systems",
+                   "username"  : "Username",
+                   "password"  : "Password",
+                   "login"     : "Login",
+                   "uniqueID"  : "Company ID",
+                   "create"    : "Create",
+                   "newcompany": "New Company",
                    }
   ,
-    LANG_SPANISH: {"hello"    : "Hola!",
-                   "title"    : "Novare Security Systems",
-                   "username" : "Usuario",
-                   "password" : "Sena"}
+    LANG_SPANISH: {"hello"     : "Hola!",
+                   "title"     : "Novare Security Systems",
+                   "username"  : "Usuario",
+                   "password"  : "Sena",
+                   "login"     : "Login",
+                   "uniqueID"  : "ID Compania",
+                   "create"    : "Criar",
+                   "newcompany": "Compania Nueva"} 
 };
 
 
--- authfe/lib/views/loginwidget.dart
+++ authfe/lib/views/loginwidget.dart
@@ -13,10 +13,18 @@ class _LoginState extends State<LoginWidget> {
   String _language = LANG_ENGLISH;
   String _userNameText;
   String _passwordText; 
+  String _appName;
+  String _loginText;
+  String _uniqueCompanyID;
+  String _newCompany;
 
   _LoginState(this._language) {
     this._userNameText = getText("username", _language);
     this._passwordText = getText("password", _language);
+    this._appName = getText("title", _language);
+    this._loginText = getText("login", _language);
+    this._uniqueCompanyID = getText("uniqueID", _language);
+    this._newCompany = getText("newcompany", _language);
   }
 
 
@@ -32,12 +40,33 @@ class _LoginState extends State<LoginWidget> {
             borderRadius: BorderRadius.all(Radius.circular(10.0))
             ),
           width: 400.0,
-          height: 400.0,
+          height: 505.0,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
               Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                child: Text(this._appName,
+                        style: TextStyle(fontSize: 26, color: Colors.white)),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+                child: Text(this._uniqueCompanyID,
+                  style: TextStyle(fontSize: 22, color:Colors.white)),
+              ),
+              Container(
                 padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
+                child: TextField(
+                  decoration: InputDecoration( 
+                    border: OutlineInputBorder(),
+                    fillColor: Colors.white,
+                    filled: true,
+                  ),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
                 child: Text(this._userNameText,
                   style: TextStyle(fontSize: 22, color:Colors.white)),
               ),
@@ -46,11 +75,13 @@ class _LoginState extends State<LoginWidget> {
                 child: TextField(
                   decoration: InputDecoration( 
                     border: OutlineInputBorder(),
+                    fillColor: Colors.white,
+                    filled: true,
                   ),
                 ),
               ),
               Container(
-                padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 10.0),
+                padding: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 20.0),
                 child: Text(this._passwordText,
                   style: TextStyle(fontSize: 22, color: Colors.white)),
               ),
@@ -58,6 +89,40 @@ class _LoginState extends State<LoginWidget> {
                 obscureText: true,
                 decoration: InputDecoration(
                   border: OutlineInputBorder(),
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+              ),
+              Divider(
+                color: Colors.white,
+                thickness: 1.0,
+              ),
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                        padding: EdgeInsets.all(5.0),
+                        child: OutlineButton(
+                          textColor: Colors.white,
+                          child: Text(this._loginText, style: TextStyle(color: Colors.white, fontSize: 16.0),),
+                          onPressed: () { print ("Testing It"); },
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),)
+                        ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(5.0),
+                      child: OutlineButton(
+                        textColor: Colors.white,
+                        onPressed: () { print("Testing it again"); },
+                        child: Text(this._newCompany, style: TextStyle(color: Colors.white, fontSize: 16.00),),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                      )
+                    ),
+                  ],
                 ),
               ),
             ],
